b03c59360717e9f83b89e7d1164bbb753d4eaeae
siyoon <siyoon210@gmail.com>
2020-06-24 22:53:00 +0900

    webapp 디렉토리 하위에 있는 정적파일 요청시에 해당 파일 보여주기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +13,7 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -22,10 +23,37 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            Map<String, String> requestMessage = new HashMap<>();
+            String line;
+            while (!"".equals(line = bufferedReader.readLine())) {
+                if (Objects.isNull(line)) {
+                    break;
+                }
+
+                if (requestMessage.isEmpty()) {
+                    final String[] s = line.split(" ");
+                    requestMessage.put("Method", s[0]);
+                    requestMessage.put("Path", s[1]);
+                    requestMessage.put("Version", s[2]);
+                    continue;
+                }
+                
+                final int i = line.indexOf(":");
+                requestMessage.put(line.substring(0, i), line.substring(i + 2));
+            }
+
+            for (Map.Entry<String, String> stringStringEntry : requestMessage.entrySet()) {
+                log.info("key '{}' value '{}'", stringStringEntry.getKey(), stringStringEntry.getValue());
+            }
+
+            final byte[] body = Files.readAllBytes(new File("./webapp" + requestMessage.get("Path")).toPath());
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+//            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
