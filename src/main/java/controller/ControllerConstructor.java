1d1272e7472a21f05e0ff076b7916fdd744c7c8d
siyoon <siyoon210@gmail.com>
2020-06-25 10:55:32 +0900

    요구사항2 GET 방식으로 회원가입하기

--- src/main/java/controller/ControllerConstructor.java
+++ src/main/java/controller/ControllerConstructor.java
@@ -8,13 +8,20 @@ public class ControllerConstructor {
     private final static Map<String, Controller> pathAndControllers = new HashMap<>();
     static {
         pathAndControllers.put("/", MainController.getInstance());
+        pathAndControllers.put("/user/create", UserCreateController.getInstance());
     }
 
     public static Controller getController(String path) {
+        if (path.contains("?")) {
+            path = path.split("\\?")[0];
+        }
+
         final Controller controller = pathAndControllers.get(path);
+
         if (Objects.isNull(controller)) {
             return DefaultController.getInstance();
         }
+
         return controller;
     }
 }
