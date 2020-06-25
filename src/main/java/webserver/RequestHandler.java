f1fc1ad110bc45c7f222dbc03396a5ee55d69bbf
siyoon <siyoon210@gmail.com>
2020-06-25 09:22:35 +0900

    RequestHandler가 Controller에 의존하도록 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import controller.Controller;
+import controller.MainController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,57 +27,8 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream();
              OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-            Map<String, String> requestMessage = new HashMap<>();
-            String line;
-            while (!"".equals(line = bufferedReader.readLine())) {
-                if (Objects.isNull(line)) {
-                    break;
-                }
-
-                if (requestMessage.isEmpty()) {
-                    final String[] s = line.split(" ");
-                    requestMessage.put("Method", s[0]);
-                    requestMessage.put("Path", s[1]);
-                    requestMessage.put("Version", s[2]);
-                    continue;
-                }
-                
-                final int i = line.indexOf(":");
-                requestMessage.put(line.substring(0, i), line.substring(i + 2));
-            }
-
-            for (Map.Entry<String, String> stringStringEntry : requestMessage.entrySet()) {
-                log.info("key '{}' value '{}'", stringStringEntry.getKey(), stringStringEntry.getValue());
-            }
-
-            final byte[] body = Files.readAllBytes(new File("./webapp" + requestMessage.get("Path")).toPath());
-
-            DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            Controller c = new MainController();
+            c.process(in, out);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
