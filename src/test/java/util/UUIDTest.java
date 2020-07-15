9b8400443fab960fd5bc73b4115c2b7b6e0cdd59
siyoon <siyoon210@gmail.com>
2020-07-15 09:32:58 +0900

    uuid 생성 테스트

---
+++ src/test/java/util/UUIDTest.java
@@ -0,0 +1,12 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class UUIDTest {
+    @Test
+    public void uuid() {
+        System.out.println("UUID.randomUUID() = " + UUID.randomUUID());
+    }
+}
